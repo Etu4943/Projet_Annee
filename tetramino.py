53c2983efffaf685ac1044d9baff8d7d8343678a
Clem <clem@MBP-de-Clem.lan>
2023-12-01 01:22:03 +0100

    Tout les test sont aux vert

--- tetramino.py
+++ tetramino.py
@@ -1,22 +1,36 @@
 import sys
 
-def create_grid(w,h):
-    grid = []
+POSITION_INDEX = 0
+COLOR_INDEX = 1
+GAP_INDEX = 2
+
+def get_w_and_h(grid):
+    return (len(grid[0])-2)//3,(len(grid)-2)//3
+
+def place_square(grid):
+    w,h = get_w_and_h(grid)
     for i in range(3*h+2):
-        line = []
         for j in range(3*w+2):
-                if i == h and j >= w+1 and j <= 2*w:
-                    line.append("--")
-                elif i > h and i < 2*h+1 and j == w:
-                    line.append(" |")
-                elif i > h and i < 2*h+1 and j == 2*w+1:
-                    line.append("| ")
-                elif i == 2*h+1 and j >= w+1 and j <= 2*w:
-                    line.append("--")
-                else :
-                    line.append("  ")
-        grid.append(line)
+            if i == h and j >= w+1 and j <= 2*w:
+                grid[i][j] = "--"
+            elif i > h and i < 2*h+1 and j == w:
+                grid[i][j] = " |"
+            elif i > h and i < 2*h+1 and j == 2*w+1:
+                grid[i][j] = "| "
+            elif i == 2*h+1 and j >= w+1 and j <= 2*w:
+                grid[i][j] = "--"
+
+def center_corner(grid):
+    w,h = get_w_and_h(grid)
+    
+    
+
+def create_grid(w,h):
+    grid = [["  " for _ in range(3*w+2)] for _ in range(3*h+2)]
+    place_square(grid)
     return grid
+
+
 
 def import_card(file_path):
     
@@ -27,40 +41,73 @@ def import_card(file_path):
     w,h = int(size[0]),int(size[1])
     shapes = []
     for line in doc :
-        shape = {}
-        shape["position"] = set()
+        shape = []
+        positions = []
         elements = line.strip().split(";")
         i = 0 
         while elements[i] != "":
-            positions = elements[i][1:-1].strip().split(",")
-            x,y = int(positions[0]),int(positions[1])
-            shape["position"].add((x,y))
+            coordinate = elements[i][1:-1].strip().split(",")
+            x,y = int(coordinate[0]),int(coordinate[1])
+            positions.append((x,y))
             i += 1
-        shape["color"] = ";".join([i for i in elements[-3:]])
+        shape.append(positions)
+        shape.append(";".join([i for i in elements[-3:]]))
+        shape.append((0,0))
         shapes.append(shape)
+        
     return ((w,h),shapes)
+
+def empty_grid(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if not "|" in grid[i][j] and not "-" in grid[i][j]:
+                grid[i][j] = "  "
+    place_square(grid)
+
+def place_tetraminos(tetraminos,grid):
+    w,h = get_w_and_h(grid)
+    empty_grid(grid)
+    nb_shapes = 0
+    for shape in tetraminos :
+        for x,y in shape[POSITION_INDEX] :
+            x,y = y + shape[GAP_INDEX][1],x + shape[GAP_INDEX][0] #Inversion de l'axe x et y
+            if(grid[x][y] != "  "):
+                grid[x][y] = f"\x1b[{shape[COLOR_INDEX]}mXX\x1b[0m"
+            else:
+                grid[x][y] = f"\x1b[{shape[COLOR_INDEX]}m{nb_shapes+1} \x1b[0m"
+        nb_shapes += 1
+    return grid
 
 def setup_tetraminos(tetraminos,grid):
     """
     \x1b[’ + code_couleur + ’m’ + texte + ’\x1b[0m
     """
-    h,w = (len(grid)-2)//3,(len(grid[0])-2)//3
+    w,h = get_w_and_h(grid)
     nb_shapes = 0
     for shape in tetraminos :
-        new_pos = set()
-        for pos in shape["position"]:
-            x,y = pos[0],pos[1]
-            if nb_shapes < 4 :
-                x,y = pos[0] + (nb_shapes // 3)*(h+1),pos[1] + (nb_shapes % 3)*(w+1)
-            elif nb_shapes == 4 :
-                x,y = pos[0] + (nb_shapes // 3)*(h+1),pos[1] + 2*(w+1)
-            else :
-                x,y = pos[0] + ((nb_shapes+1) // 3)*(h+1),pos[1] + ((nb_shapes-2) % 3)*(w+1)
-            new_pos.add((x,y))
-            grid[x][y] = f"\x1b[{shape['color']}m{nb_shapes+1} \x1b[0m"
-        shape["position"] = new_pos
+        if nb_shapes < 4 :
+            x,y = (nb_shapes // 3)*(h+1),(nb_shapes % 3)*(w+1)
+        elif nb_shapes == 4 :
+            x,y = (nb_shapes // 3)*(h+1),2*(w+1)
+        else :
+            x,y = ((nb_shapes+1) // 3)*(h+1),((nb_shapes-2) % 3)*(w+1)
+        shape[GAP_INDEX] = (y,x)
         nb_shapes += 1
-    
+    grid = place_tetraminos(tetraminos,grid)
+    return grid,tetraminos
+
+
+
+def rotate_tetramino(tetramino,clockwise = True):
+    if clockwise :
+        for i in range(len(tetramino[POSITION_INDEX])):
+            x,y = tetramino[POSITION_INDEX][i][0],tetramino[POSITION_INDEX][i][1]
+            tetramino[POSITION_INDEX][i] = (-y,x)
+    else:
+        for i in range(len(tetramino[POSITION_INDEX])):
+            x,y = tetramino[POSITION_INDEX][i][0],tetramino[POSITION_INDEX][i][1]
+            tetramino[POSITION_INDEX][i] = (y,-x)
+    return tetramino
     
 
 def display_grid(grid):
@@ -69,12 +116,55 @@ def display_grid(grid):
             print(grid[i][j],end="")
         print()
 
+def display_tetra(grid):
+    for line in grid :
+        print(line)
+
+def check_move(tetramino, grid):
+    is_valid = True
+    if tetramino[GAP_INDEX] != (0,0) :
+        for x,y in tetramino[0]:
+            x,y = y + tetramino[GAP_INDEX][1],x + tetramino[GAP_INDEX][0]
+            if grid[x][y] != "  ":
+                is_valid = False
+    return is_valid
+
+def remplire(grid):
+    w,h = get_w_and_h(grid)
+    tetraminos = []
+    shape = []
+    pos = []
+    for i in range(len(grid)) :
+        for j in range(len(grid[0])):
+            if (j > w and j < 2*w+1 and i > h and i < 2*h+1) :
+                pos.append((j,i))
+    shape.append(pos)
+    shape.append('0;37;44')
+    shape.append((0, 0))
+    tetraminos.append(shape)
+    place_tetraminos(tetraminos,grid)
+
+
+def check_win(grid):
+    win = True
+    w,h = get_w_and_h(grid)
+    somme = 0
+    for i in range(len(grid)) :
+        for j in range(len(grid[0])):
+            if (j > w and j < 2*w+1 and i > h and i < 2*h+1) and grid[i][j] != "  ":
+                somme += 1             
+    return somme == w*h
+    
+
 def main():
     carte = sys.argv[1]
     size, tetraminos = import_card(carte)
-    grid = create_grid(*size)
-    setup_tetraminos(tetraminos,grid)
+    grid = create_grid(5,7)
+    #setup_tetraminos(tetraminos,grid)
+    #display_grid(grid)
+    remplire(grid)
     display_grid(grid)
+    print(check_win(grid))
 
 if __name__ == "__main__":
     main()
